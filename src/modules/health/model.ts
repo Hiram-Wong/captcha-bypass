1d17de30bc9834bdcb61d90cd480c4dfa50aae0c
Hiram <wy-hai@qq.com>
2026-07-01 01:58:03 +0800

    chore: more health info

--- src/modules/health/model.ts
+++ src/modules/health/model.ts
@@ -2,4 +2,6 @@ import { t } from 'elysia';
 
 import { schema } from '@/utils/response';
 
-export const healthResponseSchema = schema(t.Object({ version: t.String(), timestamp: t.Number() }));
+export const healthResponseSchema = schema(
+  t.Object({ name: t.String(), homepage: t.String(), version: t.String(), timestamp: t.Number() }),
+);
--- src/modules/health/service.ts
+++ src/modules/health/service.ts
@@ -1,11 +1,15 @@
-import { APP_VERSION } from '@/utils/appInfo';
+import { APP_NAME, APP_VERSION, WEBSITE_URL } from '@/utils/appInfo';
 
 export interface HealthInfo {
+  name: string;
+  homepage: string;
   version: string;
   timestamp: number;
 }
 
 export const getHealth = (): HealthInfo => ({
+  name: APP_NAME,
+  homepage: WEBSITE_URL,
   version: APP_VERSION,
   timestamp: Date.now(),
 });
